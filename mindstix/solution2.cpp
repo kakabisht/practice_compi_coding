6ad27d946408d1606ab29800e5dc0660a7de9578
kakabisht <kakabisht07@gmail.com>
2021-08-21 11:37:54 +0530

    solution2

--- mindstix/solution2.cpp
+++ mindstix/solution2.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+
+void findPairs(int arr[][2], int num)
+{
+    unordered_map<int, int> pa;
+    for (int i = 0; i < num; i++)
+    {
+        int fir = arr[i][0];
+        int sec = arr[i][1];
+
+        if ((pa.find(sec) != pa.end()) && (pa[sec] == fir))
+        {
+            cout << sec << " " << fir << endl;
+        }
+        else
+        {
+            pa[fir] = sec;
+        }
+    }
+}
 
 int main()
 {
+    int n;
+    cin >> n;
+    int arr[n][2];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    findPairs(arr, n);
 }
